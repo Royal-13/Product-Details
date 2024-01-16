6f1f87908e90f233238354360ed19bed65722051
Royal-13 <ashu.13dun@gmail.com>
2024-01-16 18:22:41 +0530

    price fixed

---
+++ components/MobileCarousel.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function MobileCarousel() {
+    
+  return (
+    <Slider>
+    <article className="productImage">
+        <figure className="imageCar">
+          <img id="mainImage" src="/image-product-1.jpg" alt="product" />
+        </figure>
+      </article>
+    </Slider>
+  )
+}
+
+export default MobileCarousel;
